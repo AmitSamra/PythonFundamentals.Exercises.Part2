67b4deaa09829ce88a23c8a96dfdbb5e910f724d
Amit Samra <asamra@zipcodes-MacBook-Pro.local>
2020-03-04 11:25:43 -0500

    Exercise 3

---
+++ exponentiator.py
@@ -0,0 +1,21 @@
+def exponentiate(a,b):
+	'''
+	a,b numbers
+	returns a**b
+	'''
+	return a**b
+
+def raise_to_fourth_power(x):
+	'''ddjd
+	x integer
+	Runs exponentiate with b = 2
+	'''
+	return exponentiate(x,4)
+
+print(raise_to_fourth_power(2))
+
+square = lambda y : exponentiate(y,2)
+print(square(2))
+
+cube = lambda z : exponentiate(z,3)
+print(cube(2))
